e032ae16a4cb82eb31a664e5cbbd5d375a4ae8ec
Maya Thompson <mayaandromedathompson@gmail.com>
2025-05-20 10:37:05 -0500

    changed gold constructor to match workbook requirements

--- Portfolio/src/main/java/com/pluralsight/Gold.java
+++ Portfolio/src/main/java/com/pluralsight/Gold.java
@@ -2,10 +2,18 @@ package com.pluralsight;
 
 public class Gold extends FixedAsset{
 
-    double weight;
+    private double weight;
 
-    public Gold(String name, double marketValue, double weight) {
-        super(name, marketValue);
+    public Gold(double weight) {
+        super("gold", 1000);
+        this.weight = weight;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
         this.weight = weight;
     }
 
